91a58178249245510251831ab6a4063358a67d27
ericatsu <ekaatsu@st.ug.edu.gh>
2022-11-12 15:35:28 +0000

    footer added

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,118 +1,77 @@
 import React from "react";
 import "../elements/Footer.css";
-import { Button } from "../components/Button";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
+import navlogo from "../assets/images/navlogo.png";
 
 function Footer() {
   return (
-    <div className="footer-container">
-      <section className="footer-subscription">
-        <p className="footer-subscription-heading">
-          Join the Adventure newsletter for daily updates
-        </p>
-        <p className="footer-subscription-text">
-          Sign up for our email newsletter for inspirational contents and stay
-          connected with Cvmpell.
-        </p>
-        <div className="input-areas">
+    <div className="footer-container" id="footer">
+      <div className="row">
+        <div className="col">
+          <img src={navlogo} alt="" className="logo" />
+        </div>
+        <div className="col">
+          <h3>
+            Location{" "}
+            <div className="underline">
+              <span />
+            </div>
+          </h3>
+          <p>Accra</p>
+          <p>Some Building</p>
+          <p>4th Floor</p>
+          <p className="email-id">email@email.com</p>
+          <h4>+233 589 679 5798</h4>
+        </div>
+        <div className="col">
+          <h3>
+            Services{" "}
+            <div className="underline">
+              <span />
+            </div>
+          </h3>
+          <ul>
+            <li>
+              <a href="/">My Account</a>
+            </li>
+            <li>
+              <a href="/">Promotions</a>
+            </li>
+            <li>
+              <a href="/">Wish List</a>
+            </li>
+            <li>
+              <a href="/">Terms & Condition</a>
+            </li>
+            <li>
+              <a href="/">Faq's</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col">
+          <h3>
+            Newsletter{" "}
+            <div className="underline">
+              <span />
+            </div>
+          </h3>
           <form>
-            <input
-              className="footer-input"
-              name="email"
-              type="email"
-              placeholder="Your Email"
-            />
-            <Button buttonStyle="btn--outline">Subscribe</Button>
+            <i className="far fa-envelope" />
+            <input type="email" placeholder="Enter your email" />
+            <button type="submit">
+              <i className="fas fa-arrow-right" />
+            </button>
           </form>
-        </div>
-      </section>
-      <div className="footer-links">
-        <div className="footer-link-wrapper">
-          <div className="footer-link-items">
-            <h2>About Us</h2>
-            <Link to="/sign-up">Vision</Link>
-            <Link to="/">Testimonials</Link>
-            <Link to="/">Careers</Link>
-            <Link to="/">Partners</Link>
-            <Link to="/">Media</Link>
-          </div>
-          <div className="footer-link-items">
-            <h2>Contact Us</h2>
-            <Link to="/">Contact</Link>
-            <Link to="/">Support</Link>
-            <Link to="/">Location</Link>
-            <Link to="/">Sponsorships</Link>
-          </div>
-        </div>
-        <div className="footer-link-wrapper">
-          <div class="footer-link-items">
-            <h2>Contents</h2>
-            <Link to="/">Women</Link>
-            <Link to="/">Youth</Link>
-            <Link to="/">Others</Link>
-            <Link to="/">Agency</Link>
-          </div>
-          <div className="footer-link-items">
-            <h2>Social Media</h2>
-            <Link to="/">Instagram</Link>
-            <Link to="/">Facebook</Link>
-            <Link to="/">Youtube</Link>
-            <Link to="/">Twitter</Link>
+          <div className="social-icons">
+            <i className="fab fa-facebook-f" />
+            <i className="fab fa-twitter" />
+            <i className="fab fa-instagram" />
+            <i className="fab fa-whatsapp" />
           </div>
         </div>
       </div>
-      <section className="social-media">
-        <div className="social-media-wrap">
-          <div className="footer-logo">
-            <Link to="/" className="social-logo">
-              Cvmpell
-            </Link>
-          </div>
-          <small className="website-rights">Cvmpell © 2022</small>
-          <div className="social-icons">
-            <Link
-              className="social-icon-link facebook"
-              to="/"
-              target="_blank"
-              aria-label="Facebook"
-            >
-              <i className="fab fa-facebook-f" />
-            </Link>
-            <Link
-              className="social-icon-link instagram"
-              to="/"
-              target="_blank"
-              aria-label="Instagram"
-            >
-              <i className="fab fa-instagram" />
-            </Link>
-            <Link
-              className="social-icon-link youtube"
-              to="/"
-              target="_blank"
-              aria-label="Youtube"
-            >
-              <i className="fab fa-youtube" />
-            </Link>
-            <Link
-              className="social-icon-link twitter"
-              to="/"
-              target="_blank"
-              aria-label="Twitter"
-            >
-              <i className="fab fa-twitter" />
-            </Link>
-            <Link
-              className="social-icon-link twitter"
-              to="/"
-              target="_blank"
-              aria-label="LinkedIn"
-            >
-              <i className="fab fa-linkedin" />
-            </Link>
-          </div>
-        </div>
-      </section>
+      <hr />
+      <p className="copyright">Cvmpell Ltd © 2022 - All Rights Reserved</p>
     </div>
   );
 }
